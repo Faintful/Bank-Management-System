b50a911b70995050309c7da9ef71a9485392768c
faint <faintcsr@gmail.com>
2022-09-20 15:38:49 +0200

    restarted test fixture. separating tests into two fixtures next commit. checking account class created.

---
+++ src/main/java/CheckingAccount.java
@@ -0,0 +1,16 @@
+public class CheckingAccount {
+
+    int balance;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+}
---
+++ src/test/java/CheckingAccountTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckingAccountTest {
+
+//    Create a Bank Management System which has the following components:
+//    Person (which saves first, middle, and last name, age and maybe a "social security number")
+//    Checking Account (should have a current value, methods for taking and depositing money)
+//    Opening a Checking Account should only work via a Bank and must require a certain amount of initial deposit.
+
+    //TODO: Checking Account (should have a current value, methods for taking and depositing money)
+
+    @Test
+    public void getBalance() {
+        //arrange
+        CheckingAccount checkingAccount = new CheckingAccount();
+        //act
+        //assert
+        assertEquals(0, checkingAccount.getBalance());
+    }
+
+    @Test
+    public void deposit() {
+        //arrange
+        CheckingAccount checkingAccount = new CheckingAccount();
+        //act
+        checkingAccount.deposit(100);
+        //assert
+        assertEquals(100, checkingAccount.getBalance());
+    }
+
+    @Test
+    public void withdraw() {
+        //arrange
+        CheckingAccount checkingAccount = new CheckingAccount();
+        //act
+        checkingAccount.deposit(200);
+        checkingAccount.withdraw(100);
+        //assert
+        assertEquals(100, checkingAccount.getBalance());
+    }
+
+}
